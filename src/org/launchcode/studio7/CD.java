0c274bf85cf4cb9dfbdda1e9c64fc0acc0fd88f7
Forrest Burrelsman <forrestburrelsman@gmail.com>
2021-10-14 20:07:04 -0500

    completed TODOs

--- src/org/launchcode/studio7/CD.java
+++ src/org/launchcode/studio7/CD.java
@@ -14,7 +14,4 @@ public class CD extends BaseDisc implements SpinningDisc {
     }
 
 
-    // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
-    //  need to be declared separately.
-
 }
--- src/org/launchcode/studio7/Main.java
+++ src/org/launchcode/studio7/Main.java
@@ -4,10 +4,6 @@ public class Main {
 
     public static void main(String[] args){
 
-        // TODO: Declare and initialize a CD and a DVD object.
-
-        // TODO: Call each CD and DVD method to verify that they work as expected.
-
         CD cd = new CD("CD example", 700, "CD-R", 350);
         DVD dvd = new DVD("DVD example", 4700, "DVD-R", 1450);
 
